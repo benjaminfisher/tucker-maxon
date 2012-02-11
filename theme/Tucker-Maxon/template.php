2fbb4d8b42ba1d927121dc6df2ce9debb1b18b47
Benjamin Fisher <neohell@hotmail.com>
2012-02-10 17:54:45 -0800

    working on the header

---
+++ theme/Tucker-Maxon/template.php
@@ -0,0 +1,9 @@
+<a href="<?php echo get_page_url(); ?>">
+<?php get_header(); ?>
+
+<?php get_component('tagline'); ?>
+
+<?php echo get_page_title(); ?></a>
+<?php echo get_page_content(); ?>
+
+<?php get_component('sidebar'); ?>
