90a80ef96672fd1b00d8546558c17a577e2f3a9f
Benjamin Fisher <neohell@hotmail.com>
2012-02-20 19:56:23 -0800

    Changed main content wrapper from section to div for semantic optimization

--- theme/Tucker-Maxon/template.php
+++ theme/Tucker-Maxon/template.php
@@ -12,10 +12,10 @@ include 'header.inc.php';
 if(return_page_slug() != 'index' && strlen(return_parent()) != 0) get_component('sidebar');
 ?>
 
-	<section class="content">
+	<div role="main" class="content">
 		<?php get_page_content(); ?>
 		
-	</section>
+	</div>
 	
 	<?php include 'footer.inc.php'; ?>
 	
