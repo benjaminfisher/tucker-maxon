cf45667038c7a5c1e10143e875a700b1c543122e
Justin <eraera@gmail.com>
2012-02-29 15:32:57 -0800

    updates to inner page
    hero image and sub head require I18n custom fields plugin. barebones
    css at the moment for basic placement. star icon added to left list.

--- theme/Tucker-Maxon/inner_page.php
+++ theme/Tucker-Maxon/inner_page.php
@@ -17,14 +17,25 @@
 	<?php get_header(); ?>
 	
 	<link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/inner.css" media="screen" />
+	<link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/inner_notsass.css" media="screen" />
 
 </head>
-<body id="<?php get_page_slug(); ?>" >
+<body id="<?php get_page_slug(); ?>" class="inner_page">
 	
 	<?php include 'header.inc.php'; ?>
 	
 	<section id="content">
+		<div id="left-content">
+			<img id="hero-image" src="<?php get_custom_field('hero'); ?>" />
+			<h3>Programs</h3>
+			<ul>
+				<li>Preschool</li>
+				<li>Kindergarten</li>
+				<li>Elementary</li>
+			</ul>
+		</div>
 		<h1><?php get_page_title(); ?></h1>	
+		<h2><?php get_custom_field('subhead'); ?></h2>
 			<div id="page-content">
 				<div class="page-text">
 					<?php get_page_content(); ?>
