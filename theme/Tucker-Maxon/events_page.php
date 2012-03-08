aa9132d844af92d328543fec0270e97660afe8a6
Benjamin Fisher <neohell@hotmail.com>
2012-03-07 19:38:35 -0800

    Updated events list on home and added events template

---
+++ theme/Tucker-Maxon/events_page.php
@@ -0,0 +1,14 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File: 		home.php
+* @Package:		GetSimple
+* @Action:		Tucker-Maxon theme for the GetSimple CMS
+*
+*****************************************************/
+
+include 'header.inc.php'; ?>
+
+<div role="main" class="content"><?php echo events_list() ?></div>
+
+<?php include 'footer.inc.php'; ?>
