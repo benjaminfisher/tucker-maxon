e1858ce0a1a705b0b224c3db56ceaa9548d3aa7d
Nate Goldman <nnmgoldman@gmail.com>
2012-03-07 17:56:06 -0800

    sidebar php things

--- theme/Tucker-Maxon/sidebar.inc.php
+++ theme/Tucker-Maxon/sidebar.inc.php
@@ -6,8 +6,6 @@
 ?>
 		<aside class="sidebar">
 			<img class="hero" src="<?php get_custom_field('hero'); ?>" />
-			
-			<h3><?php if (!get_parent()) : get_page_clean_title(); else: get_parent(); endif ?></h3>
 			<nav>
 				<ul>
 					<?php get_i18n_navigation(return_page_slug(), 1, 99, I18N_SHOW_NORMAL); ?>
